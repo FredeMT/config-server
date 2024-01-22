95edaaa31ba29a1e697f9e72365ca16f0e6b1fa8
FredeBr <fredebrx@gmail.com>
2024-01-22 19:48:14 -0400

    Implementação do Fluxo de Pagamento - Parte 01

--- src/main/java/com/ead/configserver/configs/WebSecurityConfig.java
+++ src/main/java/com/ead/configserver/configs/WebSecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
